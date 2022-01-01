7f08bc6a318f6455824a35f635170febb56fac66
Dave Almond <dave@almond.me.uk>
2022-01-01 20:25:11 +0000

    added favicon and analytics

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,21 @@
-$=window.jQuery,$(document).ready((function(e){e("div[data-gallery]").each((function(){console.log("found gallery"),e(this).magnificPopup({delegate:"a",type:"image",gallery:{enabled:!0}})}))}));
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************************!*\
+  !*** ./src/assets/js/gallery.js ***!
+  \**********************************/
+$ = window.jQuery;
+$(document).ready(function ($) {
+  $('div[data-gallery]').each(function () {
+    console.log("found gallery");
+    $(this).magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=gallery.js.map
